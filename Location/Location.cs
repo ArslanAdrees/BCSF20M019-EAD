987d6271cae125091724c8d76f2c26554e1e21bf
ELITEBOOK <ELITEBOOK@DESKTOP-ULS1PI6>
2023-09-30 23:46:16 -0700

    Add project files.

---
+++ Location/Location.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LocationDetails
+{
+    public class Location
+    {
+        float latitude;
+        float longitude;
+        public float LocationLatitude
+        {
+            get { return latitude; }
+            set { latitude = value; }
+   
+        }
+        public float LocationLongitude
+        {
+            get { return longitude; }
+            set { longitude = value; }
+        }
+        public void setLocation(float longi, float lati)
+        {
+            longitude = longi;
+            latitude = lati;
+        }
+    }
+}
---
+++ Vehicle/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VehicleDetails
+{
+    public class Vehicle
+    {
+        String type;
+        String model;
+        String licensePlate;
+        public string VehicleType
+        {
+            get{return type;}
+            set{type = value;} 
+        }
+        public string VehicleModel
+        {
+            get{return model;}
+            set{model = value;}
+        }
+        public string VehicleLicensePlate
+        {
+            get{return licensePlate;}
+            set{licensePlate = value;}        
+        }
+    }
+}
